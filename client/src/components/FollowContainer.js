7e22a560538ebc4f7b1dee56eccd1aa9f04c8edb
Hashslinginghasher <87284121+Hashslinginghasher@users.noreply.github.com>
2021-09-19 23:20:32 -0700

    fixed route link

--- client/src/components/FollowContainer.js
+++ client/src/components/FollowContainer.js
@@ -29,7 +29,7 @@ import ListItemText from "@material-ui/core/ListItemText";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import Avatar from "@material-ui/core/Avatar";
 import Typography from "@material-ui/core/Typography";
-import { RouteLink } from "react-router-dom";
+import { Link as RouteLink } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
