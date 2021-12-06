c98b3974225d55001e38d2121011f40f63bae6d0
Hashslinginghasher <87284121+Hashslinginghasher@users.noreply.github.com>
2021-12-05 17:40:23 -0800

    tr fix

--- client/src/pages/Nft.js
+++ client/src/pages/Nft.js
@@ -1,10 +1,9 @@
-import React from "react";
-import Box from "@material-ui/core/Box";
-import { makeStyles } from "@material-ui/core/styles";
-import { styled } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import Grid from "@material-ui/core/Grid";
-
+// import React from "react";
+// import Box from "@material-ui/core/Box";
+// import { makeStyles } from "@material-ui/core/styles";
+// import { styled } from "@material-ui/core/styles";
+// import Paper from "@material-ui/core/Paper";
+// import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -13,17 +12,17 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: "column",
     alignItems: "center",
   },
-  img:{
+  img: {
     width: 500,
     minWidth: 320,
     padding: 20,
-  }
+  },
 }));
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: 'center',
+  textAlign: "center",
   color: theme.palette.text.secondary,
 }));
 
@@ -33,13 +32,22 @@ export default function BasicGrid() {
     <Box sx={{ flexGrow: 0 }}>
       <Grid container spacing={0}>
         <Grid item xs={12} sm={4}>
-          <img className={classes.img} src="https://i.imgur.com/MYmm7E1.jpeg"></img>
+          <img
+            className={classes.img}
+            src="https://i.imgur.com/MYmm7E1.jpeg"
+          ></img>
         </Grid>
         <Grid item xs={12} sm={4}>
-        <img className={classes.img} src="https://i.imgur.com/A3HI0Xm.jpeg"></img>
+          <img
+            className={classes.img}
+            src="https://i.imgur.com/A3HI0Xm.jpeg"
+          ></img>
         </Grid>
         <Grid item xs={12} sm={4}>
-        <img className={classes.img} src="https://i.imgur.com/MYmm7E1.jpeg"></img>
+          <img
+            className={classes.img}
+            src="https://i.imgur.com/MYmm7E1.jpeg"
+          ></img>
         </Grid>
       </Grid>
     </Box>
