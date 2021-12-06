f302578a791d7b6c8443eb3e7def767ff9951fd2
Patrick Wetherbee <pwetherb96@gmail.com>
2021-12-05 17:41:16 -0800

    added stuff back

--- client/src/pages/Nft.js
+++ client/src/pages/Nft.js
@@ -1,9 +1,9 @@
-// import React from "react";
-// import Box from "@material-ui/core/Box";
-// import { makeStyles } from "@material-ui/core/styles";
-// import { styled } from "@material-ui/core/styles";
-// import Paper from "@material-ui/core/Paper";
-// import Grid from "@material-ui/core/Grid";
+import React from "react";
+import Box from "@material-ui/core/Box";
+import { makeStyles } from "@material-ui/core/styles";
+import { styled } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
