1332b841f099edd70e1e37c638b8f937fd551d7b
Patrick Wetherbee <pwetherb96@gmail.com>
2022-01-12 23:14:31 -0800

    styling

--- client/src/pages/Nft.js
+++ client/src/pages/Nft.js
@@ -10,7 +10,7 @@ import { Typography } from "@material-ui/core";
 import { useParams } from "react-router-dom";
 import { fetchOBJKTDetails, generateThumbnailCR } from "../helpers/hicdex.js";
 import { query } from "../helpers/api.js";
-import { ImageGroup, Image } from 'react-fullscreen-image'
+import { ImageGroup, Image } from "react-fullscreen-image";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -38,7 +38,7 @@ const useStyles = makeStyles((theme) => ({
     paddingTop: 20,
     height: "70vmin",
   },
-  
+
   footer: {
     display: "flex",
     justifyContent: "left",
@@ -58,7 +58,6 @@ const useStyles = makeStyles((theme) => ({
     marginLeft: 20,
   },
 }));
-
 
 export default function Public() {
   const [error, setError] = useState(false);
@@ -92,67 +91,69 @@ export default function Public() {
   // const gg = ("https://ipfs.io/ipfs/" +((nft.artifact_uri).split("//").slice(-1)[0]));
   return (
     <Container>
-    <Grid item xs={12} sm={12}>
-      <Paper className={classes.paper}>
-        <img
-          className={classes.img}
-          src={nft.display_uri && generateThumbnailCR(nft.display_uri)}
-        />
-      </Paper>
-      <Paper className={classes.paper}>
-      <video width="750" height="500" controls >
-      <source src={nft.artifact_uri && generateThumbnailCR(nft.artifact_uri)} type="video/mp4"/>
-      </video>
-      </Paper>
-      <Toolbar className={classes.footer}>
-        <Typography variant="h3">{nft.title}</Typography>
-      </Toolbar>
-      <Toolbar className={classes.footer}>
-        <Typography variant="comment">{nft.description}</Typography>
-      </Toolbar>
-      <Toolbar className={classes.address}>
-        <Typography className={classes.footer} variant="comment">
-          by {nft.creator?.name || nft.creator?.address}
-        </Typography>
-      </Toolbar>
-      {comments?.map((comment) => (
-        <Comment
-          comment={comment}
-          secret={null}
-          handleDeleteComment={handleDeleteComment}
-        />
-      ))}
-
-
-          <TextField
-            error={error}
-            helperText={helperText}
-            className={classes.comment}
-            id="outlined-textarea"
-            label="Reply"
-            value={postCommentText}
-            onInput={(e) => {
-              setError(false);
-              setHelperText("");
-              setPostCommentText(e.target.value);
-            }}
-            placeholder="Enter your comment here"
-            multiline
-            fullWidth
-            variant="outlined"
+      <Grid item xs={12} sm={12}>
+        <Paper className={classes.paper}>
+          <img
+            className={classes.img}
+            src={nft.display_uri && generateThumbnailCR(nft.display_uri)}
           />
-          <Container className={classes.submitBox}>
-            <Button
-              disabled={!postCommentText.length}
-              variant="contained"
-              color="primary"
-              onClick={handleSubmitComment}
-            >
-              Submit Comment
-            </Button>
-            {/* <SecretBox updateSecret={updateSecret} secret={secret} /> */}
-          </Container>
-    </Grid>
+        </Paper>
+        <Paper className={classes.paper}>
+          <video width="750" height="500" controls>
+            <source
+              src={nft.artifact_uri && generateThumbnailCR(nft.artifact_uri)}
+              type="video/mp4"
+            />
+          </video>
+        </Paper>
+        <Toolbar className={classes.footer}>
+          <Typography variant="h3">{nft.title}</Typography>
+        </Toolbar>
+        <Toolbar className={classes.footer}>
+          <Typography variant="comment">{nft.description}</Typography>
+        </Toolbar>
+        <Toolbar className={classes.address}>
+          <Typography className={classes.footer} variant="comment">
+            by {nft.creator?.name || nft.creator?.address}
+          </Typography>
+        </Toolbar>
+        {comments?.map((comment) => (
+          <Comment
+            comment={comment}
+            secret={null}
+            handleDeleteComment={handleDeleteComment}
+          />
+        ))}
+
+        <TextField
+          error={error}
+          helperText={helperText}
+          className={classes.comment}
+          id="outlined-textarea"
+          label="Reply"
+          value={postCommentText}
+          onInput={(e) => {
+            setError(false);
+            setHelperText("");
+            setPostCommentText(e.target.value);
+          }}
+          placeholder="Enter your comment here"
+          multiline
+          fullWidth
+          variant="outlined"
+        />
+        <Container className={classes.submitBox}>
+          <Button
+            disabled={!postCommentText.length}
+            variant="contained"
+            color="primary"
+            onClick={handleSubmitComment}
+          >
+            Submit Comment
+          </Button>
+          {/* <SecretBox updateSecret={updateSecret} secret={secret} /> */}
+        </Container>
+      </Grid>
     </Container>
   );
 }
