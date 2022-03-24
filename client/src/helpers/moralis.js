220d71fcce0d206cd74d4cf9129b6ba08139f894
colindharrington <colindavidharrington@gmail.com>
2022-03-24 14:36:04 -0700

    new api just added link

--- client/src/helpers/moralis.js
+++ client/src/helpers/moralis.js
@@ -1,2 +1,18 @@
 // https://admin.moralis.io/web3Api#
 // /{address}/nft
+
+async function fetchGraphQL(operationsDoc, operationName, variables) {
+  const result = await fetch(
+    "https://api.tzstats.com/explorer/account/tz3RDC3Jdn4j15J7bBHZd29EUee9gVB1CxD9",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        query: operationsDoc,
+        variables: variables,
+        operationName: operationName,
+      }),
+    }
+  );
+  console.log(result);
+  return await result.json();
+}
